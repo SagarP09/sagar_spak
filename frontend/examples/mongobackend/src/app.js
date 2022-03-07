bbcdbca34438b4b82ad185f2701c8307e0128157
SagarP09 <sagarpethe1998@gmail.com>
2022-03-07 17:59:24 +0530

    Task1 added

---
+++ frontend/examples/mongobackend/src/app.js
@@ -0,0 +1,21 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+require("./db/conn");
+
+const port = process.env.PORT || 3000;
+
+const static_path = path.join(__dirname, "../public");
+const template_path = path.join(__dirname, "../templates/views");
+
+app.use(express.static(static_path));
+app.set("view engine", "hbs");
+app.set("views", template_path);
+
+app.get("/",(req, res) => {
+    res.render("index")
+});
+
+app.listen(port, () =>{
+    console.log(`Server is running at port no ${port}`);
+})
